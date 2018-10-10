5288aaae58162ec560bf8a9ee7c544c95694d55a
admin <lugansk_intro@mail.ru>
2018-10-10 20:08:52 +0300

    First commit

--- User.java
+++ User.java
@@ -1,4 +1,19 @@
 package lesson9;
 
 public class User {
+    private long id;
+    private String name;
+    private String sessionId;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
 }
